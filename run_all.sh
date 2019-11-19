c3c4a80db3568e23684592572144731c4c328527
Andy Hartley <andrew.hartley@metoffice.gov.uk>
2019-11-19 17:54:55 +0700

    Added conda_setup.sh
    The thinking behind this is that you shouldn't need to setup your environment before running the code. It should just load the scitools env and install the required packages

--- run_all.sh
+++ run_all.sh
@@ -33,17 +33,17 @@ eventname=${event_region_name}'/'$(echo ${end} | awk '{print substr($0,0,8)}')'_
 # Plot GPM animation for different time aggregations
 python nrt_plots_v3_casestudies.py ${start} ${end} ${event_domain} ${eventname} # TODO : make this script work in this environment
 
-## Get UM model data from FTP
-#python downloadUM.py ${organisation} # TODO : Either download from UKMO ftp site, or find files locally
-#
-## Plot postage stamps of GPM vs models
-#python plot_timelagged.py ${start} ${end} ${event_domain} ${eventname} ${organisation} # TODO : make this script work in this environment - could also be adapted for other satellite obs / analysis
-#
-## Plot SYNOP data from each organisation vs models
-#python plot_synop.py ${organisation} ${start} ${end} ${station_id} # Note: station_id is optional
-#
-## Plot Upper Air soundings for each organisation vs models
-#python plot_tephi.py
-#
-## Make an html page summarising all of the output plots
-#python make_html.py ${organisation} # TODO use code from plot_timelagged to auto-generate a summary html page
+# Get UM model data from FTP
+python downloadUM.py ${organisation} # TODO : Either download from UKMO ftp site, or find files locally
+
+# Plot postage stamps of GPM vs models
+python plot_timelagged.py ${start} ${end} ${event_domain} ${eventname} ${organisation} # TODO : make this script work in this environment - could also be adapted for other satellite obs / analysis
+
+# Plot SYNOP data from each organisation vs models
+python plot_synop.py ${organisation} ${start} ${end} ${station_id} # Note: station_id is optional
+
+# Plot Upper Air soundings for each organisation vs models
+python plot_tephi.py
+
+# Make an html page summarising all of the output plots
+python make_html.py ${organisation} # TODO use code from plot_timelagged to auto-generate a summary html page
