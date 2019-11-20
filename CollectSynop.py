70056e61334e79167ff622de52f6632eebe529f0
achmadrifani <achmadrifani@users.noreply.github.com>
2019-11-20 22:14:46 +0700

    it's working now

--- CollectSynop.py
+++ CollectSynop.py
@@ -12,7 +12,7 @@ synop temp and pilot data then store all the data in such a database
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days) + 1):
-        yield start_date + dt.timedelta(n)
+        yield start_date + timedelta(n)
 
 
 def getYMD(indate):
@@ -22,43 +22,58 @@ def getYMD(indate):
     return (year, month, day)
 
 
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
 def main(start_date, end_date, obstype, agency):
     settings = config.load_location_settings(agency)
-    start_date = datetime.strptime(start_date,'%Y%m%d')
-    end_date = datetime.strptime(end_date,'%Y%m%d')
+    start_date = datetime.strptime(str(start_date), '%Y%m%d')
+    end_date = datetime.strptime(str(end_date), '%Y%m%d')
 
     outdir = settings["datadir"]
     syn_times = ['{:02d}'.format(i) for i in range(0, 24, 3)]
     temp_times = ['{:02d}'.format(i) for i in range(0, 24, 12)]
 
-    for single_date in daterange(start_date,end_date):
+    for single_date in daterange(start_date, end_date):
         year, month, day = getYMD(single_date)
 
         syn_dir = outdir + "/synop/" + year + month + day
-        temp_dir = outdir + "/temp/" +year + month+ day
+        temp_dir = outdir + "/temp/" + year + month + day
 
-        if obstype == 'synop' :
-            for time in syn_times :
-                url = settings['db_dir']+'user='+settings['db_uname']+'&mode=web&dateRef='+year+month+date+time+'0000&timeDepth=1H&obsType=SYNOP&param=TH-PMER-TD-T-N-WIND-HU-TEND-WW_symb-VISI-CL_symb-CM_symb-CH_symb-RAF1-RAF2-TN12-TX12-TMIN10&level=0GRND&format=csv&output=binary&domain=Indonesia'
+        if not os.path.isdir(syn_dir):
+            mkdir_p(syn_dir)
+        if not os.path.isdir(temp_dir):
+            mkdir_p(temp_dir)
+
+        if obstype == 'synop':
+            for time in syn_times:
+                url = settings['db_link'] + 'user=' + settings['db_uname'] + '&mode=web&dateRef=' + year + month + day + time + '0000&timeDepth=1H&obsType=SYNOP&param=TH-PMER-TD-T-N-WIND-HU-TEND-WW_symb-VISI-CL_symb-CM_symb-CH_symb-RAF1-RAF2-TN12-TX12-TMIN10&level=0GRND&format=csv&output=binary&domain=Indonesia'
                 r = requests.get(url)
-                open(syn_dir+"/SYNOP"+year+month+date+time+".csv",'wb').write(r.content)
-        if obstype == 'temp' :
-            for time in temp_times :
-                url = settings['db_dir']+'user='+settings['db_uname']+'&mode=web&dateRef='+year+month+date+time+'0000&timeDepth=1H&obsType=TEMP&param=TH-T-TD-HU-Z-WIND-TURBUL&level=1050HPA-1000HPA-950HPA-900HPA-850HPA-800HPA-700HPA-600HPA-500HPA-400HPA-300HPA-250HPA-200HPA-150HPA-100HPA-70HPA-50HPA&format=csv&output=binary&domain=Indonesia'
+                open(syn_dir + "/SYNOP" + year + month + day + time + ".csv", 'wb').write(r.content)
+        if obstype == 'temp':
+            for time in temp_times:
+                url = settings['db_link'] + 'user=' + settings['db_uname'] + '&mode=web&dateRef=' + year + month + day + time + '0000&timeDepth=1H&obsType=TEMP&param=TH-T-TD-HU-Z-WIND-TURBUL&level=1050HPA-1000HPA-950HPA-900HPA-850HPA-800HPA-700HPA-600HPA-500HPA-400HPA-300HPA-250HPA-200HPA-150HPA-100HPA-70HPA-50HPA&format=csv&output=binary&domain=Indonesia'
                 r = requests.get(url)
-                open(temp_dir + "/TEMP" + year + month + date + time + ".csv", 'wb').write(r.content)
+                open(temp_dir + "/TEMP" + year + month + day + time + ".csv", 'wb').write(r.content)
 
 
 if __name__ == '__main__':
-    print("Downloading Synop / TEMP Observation Data")
+    print("Downloading SYNOP / TEMP Observation Data")
     print("Script starts at " + str(datetime.now()))
 
     start_date = sys.argv[1]  # date should in YYYYMMDD
     end_date = sys.argv[2]  # start time is HH in UTC
-    obstype = sys.argv[3] # synop, temp
-    agency = sys.argv[4] # BMKG
+    obstype = sys.argv[3]  # synop, temp
+    agency = sys.argv[4]  # BMKG
 
-    if len(start_date) < 8 or len(end_date)<8 :
+    if len(start_date) < 8 or len(end_date) < 8:
         print("Date should be in YYYYMMDD format")
         quit()
     if start_date > end_date:
